1e3f8a955857d9c0e4edf338848aa25ea037591c
haileliya <hailelt@alumni.vcu.edu>
2022-03-19 10:38:25 -0400

    first push

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+#Set up Fask
+app = Flask(__name__)
+
+#Use flask_pymongo to set up mongo connection
+#Below tells Python that our app will onnect ot Mongo using a URI.
+#This URI is saying that the app can reach Mongo through our localhost server, using port 27017
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+#tells Flask what to display when we're looking at the home page, index.html (index.html is the default HTML file that we'll use to display the content we've scraped). 
+@app.route("/")
+def index():
+   #uese pymongo to find the mars collectionin our database, 
+   mars = mongo.db.mars.find_one()
+   #tells flask to reurn an HTML template using an index.html file (which ill be created after we build the fask route)
+   #mars=mars, tells python to use the mars collection in mongodb
+   return render_template("index.html", mars=mars)
+
+#Defines the route that flask will be using
+@app.route("/scrape")
+def scrape():
+   #assign a new variable that points to our mongo database
+   mars = mongo.db.mars
+   #assign a new variable that holds the newly scraped data
+   mars_data = scraping.scrape_all()
+   #this line updates the database
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   #redirect after successfully scraping the data and navigate our page back to / where we can see the updated content.
+   return redirect('/', code=302)
+#code that tells falsk to run
+if __name__ == "__main__":
+   app.run()
---
+++ scraping.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+# In[2]:
+
+#set your executable path in the next cell
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+# In[3]:
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+#Secondly, we're also telling our browser to wait one second before searching for components. 
+#The optional delay is useful because sometimes dynamic pages take a little while to load,
+
+# In[9]:
+
+#Setup the HTML parser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('div.list_text')
+
+# In[10]:
+
+#Notice how we've assigned slide_elem as the variable to look for the <div /> tag and its descendent 
+#(the other tags within the <div /> element)? This is our parent element. 
+#This means that this element holds all of the other elements within it, and we'll reference it when we want to 
+#filter search results even further. The . is used for selecting classes, such as list_text, 
+#so the code 'div.list_text' pinpoints the <div /> tag with the class of list_text. CSS works from right to left, 
+#such as returning the last item on the list instead of the first. 
+#Because of this, when using select_one, the first matching element returned will be a <li /> element with a 
+#class of slide and all nested elements within it.
+
+# In[11]:
+
+#HTML attribute will we use to scrape the article’s title
+#class="content_title"
+
+# In[12]:
+
+slide_elem.find('div', class_='content_title')
+
+# In[13]:
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+#With this new code, we’re searching within that element for the title. We’re also stripping the additional 
+#HTML attributes and tags with the use of .get_text().
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+# In[15]:
+
+#add the summary text.
+news_summary = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_summary
+
+# ###Featured Images
+
+# In[16]:
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+# In[17]:
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[ ]:
+
+
+#The code above automatically clicked the full image. 
+#Now we need to parse it so we can otniue  to scrape the full-size image URL
+
+
+# In[18]:
+
+
+#Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# In[19]:
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+# In[20]:
+
+# Use the base URL to create an absolute URL
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+#We're using an f-string for this print statement because it's a cleaner way to create print statements; 
+#they're also evaluated at run-time.
+
+
+# ###Mars FACTS
+
+# In[ ]:
+
+#Instead of scraping each row, or the data in each <td />, 
+#we're going to scrape the entire table with Pandas' .read_html() function.
+
+# In[23]:
+
+#With this line, we're creating a new DataFrame from the HTML table. 
+#By specifying an index of 0, we're telling Pandas to pull only the first table it encounters, or the first item in the list. 
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+#assign columns to the new DataFrame for additional clarity.
+df.columns=['description', 'Mars', 'Earth']
+#turning the Description column into the DataFrame's index. inplace=True means that 
+#the updated index will remain in place, without having to reassign the DataFrame to a new variable.
+df.set_index('description', inplace=True)
+df
+
+# In[24]:
+
+#How do we add the DataFrame to a web application? and make sure that is updated live when the html is updated
+#use the .to_html() function 
+
+df.to_html()
+
+# In[25]:
+
+browser.quit()
+
+# In[ ]:
+
+
+
+
